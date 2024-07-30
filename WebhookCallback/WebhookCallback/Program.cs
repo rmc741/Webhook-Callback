7ecff7412636f360e9fd680c43bdcf6cec7fd9b3
Rafael Mendonca <rafael.medonca2@gmail.com>
2024-07-30 08:37:28 -0300

    callback do webhook

--- WebhookCallback/WebhookCallback/Program.cs
+++ WebhookCallback/WebhookCallback/Program.cs
@@ -1,4 +1,17 @@
+
+const string server = "http://localhost:5003";
+const string callback = "http://localhost:5004/wh/item/new";
+const string topic = "item.new";
+
+var client = new HttpClient();
+
+Console.WriteLine($"Subscribing to topic {topic} with callback {callback}");
+await client.PostAsJsonAsync(server + "/subscribe", new { topic, callback });
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddLogging();
 
 // Add services to the container.
 
@@ -8,6 +21,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+pp.MapPost("/wh/item/new", (object payload, ILogger<Program> logger) =>
+{
+    logger.LogInformation("Received payload: {payload}", payload);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
